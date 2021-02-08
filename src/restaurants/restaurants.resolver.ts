ac4686e7ea2e83e91104f13b6eeaa4f6a75c054c
wolfdate25 <wolfdate25@naver.com>
2021-02-08 11:34:35 +0900

    create restaurant dto, mutation

--- src/restaurants/restaurants.resolver.ts
+++ src/restaurants/restaurants.resolver.ts
@@ -1,9 +1,18 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { Restaurant } from './entities/restaurant.entity';
+import { createRestaurantDto } from './dtos/create-restaurant.dto';
 
 @Resolver()
 export class RestaurantsResolver {
-  @Query(() => Boolean)
-  isPizzaGood(): boolean {
+  @Query((returns) => [Restaurant])
+  myRestaurant(@Args('veganOnly') veganOnly: boolean): Restaurant[] {
+    console.log(veganOnly);
+    return [];
+  }
+
+  @Mutation((returns) => Boolean)
+  createRestaurant(@Args() createRestaurantsDto: createRestaurantDto) {
+    console.log(createRestaurantsDto);
     return true;
   }
 }
